38f793222615f5894835e46d8cfdc75418567563
nmaypeter <37822464+nmaypeter@users.noreply.github.com>
2019-06-15 18:52:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,15 +14,15 @@ if __name__ == '__main__':
             for prod_setting in prod_seq:
                 product_name = 'item_lphc' * (prod_setting == 1) + 'item_hplc' * (prod_setting == 2)
 
-                Model('mnapg', dataset_name, product_name, cascade_model, '').model_napg(r_flag=False, sr_flag=False, epw_flag=False)
-                Model('mnapgr', dataset_name, product_name, cascade_model, '').model_napg(r_flag=True, sr_flag=False, epw_flag=False)
-                Model('mnapgsr', dataset_name, product_name, cascade_model, '').model_napg(r_flag=True, sr_flag=True, epw_flag=False)
-                Model('mng', dataset_name, product_name, cascade_model, '').model_ng(r_flag=False, sr_flag=False)
-                Model('mngr', dataset_name, product_name, cascade_model, '').model_ng(r_flag=True, sr_flag=False)
-                Model('mngsr', dataset_name, product_name, cascade_model, '').model_ng(r_flag=True, sr_flag=True)
-                Model('mhd', dataset_name, product_name, cascade_model, '').model_hd()
-                Model('mr', dataset_name, product_name, cascade_model, '').model_r()
-                Model('mpmis', dataset_name, product_name, cascade_model, '').model_pmis()
+                Model('mnapg', dataset_name, product_name, cascade_model).model_napg(r_flag=False, sr_flag=False, epw_flag=False)
+                Model('mnapgr', dataset_name, product_name, cascade_model).model_napg(r_flag=True, sr_flag=False, epw_flag=False)
+                Model('mnapgsr', dataset_name, product_name, cascade_model).model_napg(r_flag=True, sr_flag=True, epw_flag=False)
+                Model('mng', dataset_name, product_name, cascade_model).model_ng(r_flag=False, sr_flag=False)
+                Model('mngr', dataset_name, product_name, cascade_model).model_ng(r_flag=True, sr_flag=False)
+                Model('mngsr', dataset_name, product_name, cascade_model).model_ng(r_flag=True, sr_flag=True)
+                Model('mhd', dataset_name, product_name, cascade_model).model_hd()
+                Model('mr', dataset_name, product_name, cascade_model).model_r()
+                Model('mpmis', dataset_name, product_name, cascade_model).model_pmis()
 
                 for wd in wd_seq:
                     wallet_distribution_type = 'm50e25' * (wd == 1) + 'm99e96' * (wd == 2)

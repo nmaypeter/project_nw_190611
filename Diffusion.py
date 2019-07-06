7c8c3da111353ea1856ed2450583bb1c7783c1e6
nmaypeter <37822464+nmaypeter@users.noreply.github.com>
2019-07-07 00:17:12 +0800

    Update Diffusion.py

--- Diffusion.py
+++ Diffusion.py
@@ -162,3 +162,36 @@ class DiffusionAccProb:
                                                 combineDict(i_dict, iv_dict)
 
         return i_dict
+
+
+class DiffusionAccProb2:
+    def __init__(self, graph_dict, seed_cost_dict, product_list):
+        ### graph_dict: (dict) the graph
+        ### product_list: (list) the set to record products [kk's profit, kk's cost, kk's price]
+        ### num_node: (int) the number of nodes
+        ### num_product: (int) the kinds of products
+        ### product_weight_list: (list) the product weight list
+        self.graph_dict = graph_dict
+        self.seed_cost_dict = seed_cost_dict
+        self.product_list = product_list
+        self.num_product = len(product_list)
+        self.prob_threshold = 0.001
+
+    def getExpectedInf(self, s_set):
+        s_total_set = set(s for k in range(self.num_product) for s in s_set[k])
+        i_dict = [{i: 0.0 for i in self.seed_cost_dict[0]} for _ in range(self.num_product)]
+
+        for k in range(self.num_product):
+            i_seq = [s for s in s_set[k] if s in self.graph_dict]
+            i_seq = [(ii, self.graph_dict[i][ii]) for i in i_seq for ii in self.graph_dict[i] if ii not in s_total_set and self.graph_dict[i][ii] >= self.prob_threshold]
+            while i_seq:
+                for i_item in i_seq:
+                    i_node, i_prod = i_item
+                    i_dict[k][i_node] = round(1.0 - (1.0 - i_dict[k][i_node]) * (1.0 - i_prod), 4)
+
+                i_seq = [i for i in i_seq if i[0] in self.graph_dict]
+                i_seq = [(ii, round(i[1] * self.graph_dict[i[0]][ii], 4)) for i in i_seq for ii in self.graph_dict[i[0]] if ii not in s_total_set and round(i[1] * self.graph_dict[i[0]][ii], 4) >= self.prob_threshold]
+
+        exp_inf = [round(sum(i_dict[k][i] for i in i_dict[k]), 4) for k in range(self.num_product)]
+
+        return exp_inf

db238f28dfb2631da44c37b6d20df43a12a7c420
nmaypeter <37822464+nmaypeter@users.noreply.github.com>
2019-07-15 21:29:23 +0800

    Update Diffusion.py

--- Diffusion.py
+++ Diffusion.py
@@ -79,35 +79,3 @@ class Diffusion:
                     a_n_sequence, a_n_sequence2 = a_n_sequence2, a_n_sequence
 
         return round(ep, 4)
-
-
-class DiffusionMDAG:
-    def __init__(self, graph_dict, product_list):
-        ### graph_dict: (dict) the graph
-        ### product_list: (list) the set to record products [kk's profit, kk's cost, kk's price]
-        ### num_node: (int) the number of nodes
-        ### num_product: (int) the kinds of products
-        ### product_weight_list: (list) the product weight list
-        self.graph_dict = graph_dict
-        self.product_list = product_list
-        self.num_product = len(product_list)
-        self.prob_threshold = 0.001
-
-    def generateNodeRankDict(self):
-        node_rank_dict = {i: {j: 0.0 for j in self.graph_dict[i]} for i in self.graph_dict}
-
-        for s in self.graph_dict:
-            print(s)
-            i_seq = [(i, self.graph_dict[s][i], [(s, i)]) for i in self.graph_dict[s] if self.graph_dict[s][i] >= self.prob_threshold]
-            while i_seq:
-                for i_item in i_seq:
-                    i_node, i_prod, i_frag = i_item
-                    for i_frag_item in i_frag:
-                        i, j = i_frag_item
-                        node_rank_dict[i][j] += i_prod
-
-                i_seq = [i for i in i_seq if i[0] in self.graph_dict]
-                i_seq = [(ii, round(i_item[1] * self.graph_dict[i_item[0]][ii], 4), i_item[2] + [(i_item[0], ii)]) for i_item in i_seq for ii in self.graph_dict[i_item[0]]
-                         if round(i_item[1] * self.graph_dict[i_item[0]][ii], 4) >= self.prob_threshold]
-
-        return node_rank_dict
